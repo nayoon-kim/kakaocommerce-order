001a8c203ab634f9f83737491d8ff4a9ec20926e
김나윤 <belloyv@gmail.com>
2021-04-12 21:32:24 +0900

    initial

---
+++ src/main/java/com/kakaocommerce/order/OrderApplication.java
@@ -0,0 +1,13 @@
+package com.kakaocommerce.order;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderApplication.class, args);
+	}
+
+}
